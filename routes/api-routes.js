efab890a139a9504a5813580c1fea34af4b9de82
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-05-25 21:09:11 -0400

    Fix 2.3

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -27,8 +27,8 @@ apiRoutes.post('/testsend', (req, res, next) => {
 });
 
 apiRoutes.post('/response', (req, res, next) => {
-  res.send('<Response><Message>Hello</Message></Response>');
-  /*
+  //res.send('<Response><Message>Hello</Message></Response>');
+  
   const messageSid      = req.body.messageSid;
   console.log(messageSid);
   http.createServer((req, res) => {
@@ -36,9 +36,9 @@ apiRoutes.post('/response', (req, res, next) => {
     twiml.message('SPR Response to your SMS.');
     console.log(twiml.message);
     res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+    res.send(twiml.toString());
   }).listen(1337, "https://radiant-forest-23151.herokuapp.com/");
-  */
+
 });
 
 apiRoutes.get('/response', (req, res, next) => {
