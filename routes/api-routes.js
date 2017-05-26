96f3971b15482c2a47a45d2a70608f040b81ce3e
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-05-25 20:11:15 -0400

    Updating response message

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,7 +30,7 @@ apiRoutes.post('/response', (req, res, next) => {});
 apiRoutes.get('/response', (req, res, next) => {
   http.createServer((req, res) => {
     var twiml             = new twilio.TwimlResponse();
-    twiml.message('Thanks for the text.');
+    twiml.message('SPR Response to your SMS.');
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
   }).listen(1337);
