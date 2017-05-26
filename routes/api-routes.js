afe9391ff0eafe8e64463bbae9779200e4a24df5
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-05-25 20:22:26 -0400

    Testing in Heroku.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -25,7 +25,14 @@ apiRoutes.post('/testsend', (req, res, next) => {
   });
 });
 
-apiRoutes.post('/response', (req, res, next) => {});
+apiRoutes.post('/response', (req, res, next) => {
+  http.createServer((req, res) => {
+    var twiml             = new twilio.TwimlResponse();
+    twiml.message('SPR Response to your SMS.');
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  }).listen(1337);
+});
 
 apiRoutes.get('/response', (req, res, next) => {
   http.createServer((req, res) => {
