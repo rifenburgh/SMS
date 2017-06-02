ab986fad74cd40a6e542a73189815d31fdc90364
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-06-02 15:15:24 -0400

    Heroku 7.2.x1

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -140,7 +140,7 @@ apiRoutes.get('/listmessages', (req, res, next) => {
 apiRoutes.get('/listmessage/:from', (req, res, next) => {
   const fromPhone             = req.params.from;
   // console.log("/listmes/from", fromPhone);
-  Message.find({ 'From': fromPhone }, (err, items) => {
+  Message.find({ 'phone': fromPhone }, (err, items) => {
     if (err) {
       next(err);
       return;
